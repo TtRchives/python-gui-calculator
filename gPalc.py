fbb579d18a22a1711252cd9490dbc4e264bbedb9
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-11-06 21:09:49 -0500

    .

--- gPalc.py
+++ gPalc.py
@@ -7,12 +7,13 @@ import tkinter.messagebox
 import tkinter.simpledialog #https://stackoverflow.com/questions/43149229/tkinter-input-and-output-in-python-3
 root = Tk()
 root.title("gPalc")
-file=tkinter.simpledialog.askstring("Number One: ", "Type the first number: ")
+def askFirstNumber():
+    num1=tkinter.simpledialog.askstring("Number One: ", "Type the first number: ")
 def values():
     print("Some text")
 output=values()
-def __init__(self, parent, controller):
-    tkinter.Frame.__init__(self, parent)
-    self.controller = controller
-    button = ttk.Button(self, text="Submit", command=tkmessagebox.showinfo("Results",output))
+#def __init__(self, parent, controller):
+#    tkinter.Frame.__init__(self, parent)
+#    self.controller = controller
+#    button = ttk.Button(self, text="Submit", command=tkmessagebox.showinfo("Results",output))
 root.mainloop()
